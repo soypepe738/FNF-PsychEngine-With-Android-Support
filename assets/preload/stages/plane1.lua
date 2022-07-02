ed211e879a9d7d82525dd19495d00fcb019222b6
soypepe738 <108590572+soypepe738@users.noreply.github.com>
2022-07-02 14:19:11 -0500

    Add files via upload

---
+++ assets/preload/stages/plane1.lua
@@ -0,0 +1,13 @@
+function onCreate()
+  --background
+  makeLuaSprite('bg','plane/limoSunset1',-300,-700)
+  scaleObject('bg',1.5,1.4)
+  setScrollFactor('bg',1,0.3)
+  makeAnimatedLuaSprite('plane','plane/limoDrive',-100,550)
+  scaleObject('plane',1.5,1)
+  addAnimationByPrefix('plane','idle','Limo stage',24,true)
+  objectPlayAnimation('plane','idle',true)
+  addLuaSprite('bg',false)
+  addLuaSprite('plane',false)
+  close(true);
+end
